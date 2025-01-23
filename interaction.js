18ef7044807ab52803695172cb927599740ac4e4
rishabh0604 <rishabhsarraf5@gmail.com>
2025-01-24 01:57:35 +0530

    interactionsAdded

---
+++ interaction.js
@@ -0,0 +1,17 @@
+//   alert, prompt, confirm
+
+// alert : It shows message and wait for the user to press "ok"
+
+alert("Hello world");
+
+// prompt : The function prompt accepts two arguments
+
+let person = prompt("Please enter your name", "Harry Potter");
+
+// confirm :The function confirm shows a modal window with a question and two buttons: OK and Cancel.
+// The result is true if OK is pressed and false otherwise.
+
+
+let isStudent = confirm("Are you the Student ?");
+
+alert( isStudent ); // true if OK is pressed
