94878b97be30d504ce5f9ecdd7769fb2656caed8
rishabh0604 <rishabhsarraf5@gmail.com>
2025-01-25 01:45:58 +0530

    basicOperatosAdded

---
+++ basicOperators.js
@@ -0,0 +1,67 @@
+/*Javascript operators are symbols which are used to assign values, compare values,  perform arthematic operations and more.Javascript
+
+>> The variables(operations) are called operands.
+>> The operation (to be performed between the two operands) is defined by an operator.*/
+
+// Addition
+//The addition operator is denoted by '+'. It is used to add two or more numbers and returns their sum as the output.
+
+let num1 = 25; 
+let num2 = 36;
+let num3 = 55;
+
+let sum = num1 + num2 + num3;
+console.log(sum);  // ouyput : 116
+
+
+// Subtraction
+//The subtraction operator is denoted by '-'. It is used to subtract one number from another and returns their difference as the output.
+
+let number1 = 25;
+let number2 = 35;
+
+let diff = number1 - number2;
+console.log(diff);  //output : -10
+
+
+// Multiplication
+//The multiplication operator is denoted by '*'. It is used to multiply one number from another and returns their product as the output.
+
+let num4 = 5;
+let num5 = 6;
+
+let prod = num4 * num5;
+console.log(prod);  // Output : 30
+
+
+// Exponential
+//The exponentiation operator is denoted by '**'. This operator raises the first operand to the power of second operand.
+ 
+let x = 2;
+let y = 5;
+
+let ans = x ** 3;
+console.log(ans);  //Output : 8
+
+ans = x ** y;
+console.log(ans);  // Output :32
+
+//Division
+//The division operator is denoted by '/'. It is used to divide one number by another and returns their quotient as the output.
+
+let a = 10;
+let b = 3;
+
+let sol = a/b;
+console.log(sol);  // Output :3.33333
+
+
+//Modulus
+//The modulus operator is denoted by '%'. It returns the division remainder i.e. the remainder of two numbers that have been divided.
+
+let mod1 = 10;
+let mod2 = 4;
+
+let remain = mod1 % mod2; 
+console.log(remain);
+//Output : 2
