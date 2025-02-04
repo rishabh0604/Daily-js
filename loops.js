047fa867f920bc103c7503e454e27e32d34c9e6c
rishabh0604 <rishabhsarraf5@gmail.com>
2025-02-04 17:45:27 +0530

    loopsAdded

---
+++ loops.js
@@ -0,0 +1,121 @@
+//  Loops: while and for
+
+// for loop
+
+// printing name 10 times in loop
+
+// for (i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// //  table in for loop
+
+// for(i=1; i<=10; i++){
+//     console.log(i*2)
+// }
+
+// // printing name in loop
+// for(let i=1; i<=10; i++){
+//     console.log("Rishabh")
+// }
+
+// // reverse counting using loop
+// for (let i=100; i>=1; i--){
+//     console.log(i)
+// }
+
+// break and continue in loop
+
+// for (let i=0; i<=6; i++){
+//     if(i==4)
+//         break;
+//     else(console.log(i));
+// }
+
+// for (let x =1; x<=10; x++){
+//     if(x == 5)
+//         continue;
+//     else console.log(x)
+// }
+
+
+// while loop
+
+//creating table using while loop
+// let i=1;
+// while(i <= 10){
+//     console.log(i*2)
+//     i++;
+// }
+
+
+// creating reverse count using while loop
+// let a = 10;
+// while(a >= 1){
+//     console.log(a)
+//     a--;
+// }
+
+
+// printing name using while loop
+// let b = 1
+// while( b<= 10){
+//     console.log("Rishabh")
+//     b++;
+// }
+
+// continue and break using while loop
+
+// let i = 0;
+// while(i<5){
+//     if (i == 3)
+//         break;
+//     else
+//        console.log(i)
+//     i++;
+// }
+
+
+
+// let i = 1;
+// while(i < 5){
+//     console.log("Inside the loop")
+//     if (i == 3){
+//         i++
+//         continue;
+//     }
+     
+//     else 
+//        console.log(i)
+//     i++;
+// }
+
+
+// do-while loop
+
+// let i = 1;
+// do{
+//     console.log("Rishabh");
+//     //updation
+//     i++;
+
+
+// printing count using do-while loop
+
+// let a = 1;
+// do{
+//     console.log(a);
+//     //updation
+//     a++
+// }while(a<=10);
+
+
+// reverse count using do-while loop 
+
+
+// let i =10;
+// do{
+//   console.log(i);
+//   //updation
+//   i--;
+// }while(i>=0);
