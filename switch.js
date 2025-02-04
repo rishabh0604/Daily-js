17979a51409fd2946a2adcf4784cb953a5553946
rishabh0604 <rishabhsarraf5@gmail.com>
2025-02-04 20:48:38 +0530

    switchAdded

---
+++ switch.js
@@ -0,0 +1,129 @@
+/* The syntax
+The switch has one or more case blocks and an optional default.
+
+It looks like this:
+
+switch(x) {
+  case 'value1':  // if (x === 'value1')
+    ...
+    [break]
+
+  case 'value2':  // if (x === 'value2')
+    ...
+    [break]
+
+  default:
+    ...
+    [break]
+} */
+
+// let a = 2 + 2;
+
+//example
+
+// switch (a) {
+//   case 3:
+//    console.log( 'Too small' );
+//     break;
+//   case 4:
+//    console.log( 'Exactly!' );
+//     break;
+//   case 5:
+//   console.log( 'Too big' );
+//     break;
+//   default:
+//     console.log( "I don't know such values" );
+// }
+
+// example :
+
+// let month = prompt("choose a month");
+// switch (month) {
+//   case "1":
+//     console.log("january");
+//     break;
+//   case "2":
+//     console.log("febuary");
+//     break;
+//   case "3":
+//     console.log("march");
+//     break;
+//   case "4":
+//     console.log("april");
+//     break;
+//   case "5":
+//     console.log("may");
+//     break;
+//   case "6":
+//     console.log("june");
+//     break;
+//   case "7":
+//     console.log("july");
+//     break;
+//   case "8":
+//     console.log("august");
+//     break;
+//   case "9":
+//     console.log("september");
+//     break;
+//   case "10":
+//     console.log("october");
+//     break;
+//   case "11":
+//     console.log("november");
+//     break;
+//   case "12":
+//     console.log("december");
+//     break;
+//   default:
+//     console.log("Invalid month!");
+// }
+
+
+// example 
+
+// let day = prompt("Choose a day");
+
+// switch (day) {
+//   case "1":
+//     document.write("Today is Monday");
+//     break;
+//   case "2":
+//     document.write("Today is Tuesday");
+//     break;
+//   case "3":
+//     document.write("Today is Wednesday");
+//     break;
+//   case "4":
+//     document.write("Today is Thrusday");
+//     break;
+//   case "5":
+//     document.write("Today is Friday");
+//     break;
+//   case "6":
+//     document.write("Today is Saturday");
+//     break;
+//   case "7":
+//     document.write("Today is Sunday");
+//     break;
+//    default :
+//    document.write("Invalid day!");
+// }
+
+
+// example
+
+//  let age = 1;
+//  switch(true){
+//     case(age >= 18 && age <= 30):
+//     document.write("You are a mature");
+//     break;
+//     case(age >= 30 && age <= 60):
+//     document.write("You are a adult");
+//     break;
+//     case(age >= 1 && age <= 18):
+//     document.write("You are a child");
+//     break;
+//     default:
+//       document.write("God bless u")
+//  }
