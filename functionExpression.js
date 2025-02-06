becbafe7c7371741eb21cec120767860e26a9c10
rishabh0604 <rishabhsarraf5@gmail.com>
2025-02-06 15:56:53 +0530

    functionExpressionAdded

---
+++ functionExpression.js
@@ -0,0 +1,36 @@
+// function sayHi() {
+//     alert( "Hello" );
+//   }
+//   sayHi()
+
+
+
+// function getMultiplication(a,b){
+//   return a*b;
+// }
+// let ans = getMultiplication(5,4);
+// console.log(ans);
+
+
+
+// let getSum = function(a,b) {
+//   return a+b;
+// }
+// let sum = getSum(15,20);
+// console.log(sum);
+
+
+// let square = function(num){
+//  let ans = num**2;
+//  return ans;
+// }
+// let ans = square(5);
+// console.log(ans);
+
+
+// let remainder = function(num1, num2){
+//  let ans = num1%num2;
+//  return ans;
+// }
+// let ans = remainder(2,5);
+// console.log(ans);
