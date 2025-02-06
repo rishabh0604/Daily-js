2b26a47674782c88b873e156d783e381216adbf1
rishabh0604 <rishabhsarraf5@gmail.com>
2025-02-06 17:24:05 +0530

    arrowFunctionAdded

---
+++ arrowFunction.js
@@ -0,0 +1,32 @@
+// let getExp = (x,y) => {
+//     let ans = x**y;
+//     return ans;
+// }
+// console.log(getExp(2,10));
+
+
+
+// let sayHi = () => console.log("Hello!");
+
+// sayHi();
+
+
+
+// let firstName = () => {
+//     console.log("rishabh")
+// }
+// firstName()
+
+
+
+// let welcome = (name) =>{
+//     return `Hello & Welcome ${name}`
+// }
+// console.log(welcome("Rishabh"));
+
+
+
+// let welcome = (name, age) =>{
+//     return `Hello & Welcome ${name} - ${age}`
+// }
+// console.log(welcome("Rishabh", 25));
